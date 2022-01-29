60a57a2536477f5895a79dec1ac15f8b2df1f54b
unknown <lidiane.007.51@gmail.com>
2022-01-29 19:30:24 -0300

    Colocando saida para um arquivo

--- algoc/algoc.c
+++ algoc/algoc.c
@@ -16,7 +16,7 @@ struct no_lista
     struct no_lista *anterior;
 };
 
-struct no_lista *add(char *comando, struct no_lista *itemAnterior)
+struct no_lista *add(FILE *arqSaida, char *comando, struct no_lista *itemAnterior)
 {
     struct no_lista *item = NULL;
     if (item == NULL)
@@ -26,6 +26,7 @@ struct no_lista *add(char *comando, struct no_lista *itemAnterior)
     if (item == NULL)
     {
         printf("\nErro!");
+        fprintf(arqSaida, "\nErro!");
         getchar();
         free(item);
         exit(EXIT_FAILURE);
@@ -36,7 +37,7 @@ struct no_lista *add(char *comando, struct no_lista *itemAnterior)
     return itemAnterior;
 }
 
-void listar(struct no_lista *item)
+void listar(FILE *arqSaida, struct no_lista *item)
 {
     struct no_lista *itemAnterior = NULL;
     /* Mostra a lista */
@@ -44,6 +45,7 @@ void listar(struct no_lista *item)
     while (itemAnterior != NULL)
     {
         printf("%s\n", itemAnterior->comando);
+        fprintf(arqSaida, "%s\n", itemAnterior->comando);
         itemAnterior = itemAnterior->anterior;
     }
 };
@@ -60,8 +62,14 @@ void limpaLista(struct no_lista *item)
     }
 };
 
-struct no_lista *trataNumero(int numero, struct no_lista *item)
+struct no_lista *trataNumero(FILE *arqSaida, int numero, struct no_lista *item)
 {
+    // começa do numero fornecido
+    // se for impar diminui 1 (usa o -1)
+    // se usar (-1) coloca um INC
+    // se usar a divisão por 2 coloca o DUP
+    // se for par divide por 2 e usa o resultado
+    // faz ate chegar em zero
     if (numero == 1 || numero == -1)
     {
         return item;
@@ -69,28 +77,35 @@ struct no_lista *trataNumero(int numero, struct no_lista *item)
     else
     {
         if (numero % 2 == 0)
-        { // he par
-            item = add("DUP", item);
-            item = trataNumero(numero / 2, item);
+        { // eh par
+            item = add(arqSaida, "DUP", item);
+            item = trataNumero(arqSaida, numero / 2, item);
         }
         else
-        { // he impar
-            item = add("INC", item);
-            item = trataNumero(--numero, item);
+        { // eh impar
+            item = add(arqSaida, "INC", item);
+            item = trataNumero(arqSaida, --numero, item);
         }
     }
 }
 void main(void)
 {
     FILE *arqEntrada;
+    FILE *arqSaida;
     char *tecla;
     struct no_lista *item = NULL;
     int constante;
 
     arqEntrada = fopen("algoc.in", "r"); // Cria um arquivo texto para leitura
-    if (arqEntrada == NULL)              // Se nào conseguiu criar
+    if (arqEntrada == NULL)              // Se nao conseguiu criar
     {
         printf("Problemas na leitura do arquivo \n");
+        return;
+    }
+    arqSaida = fopen("algoc.out", "w"); // Cria um arquivo texto para gravacao
+    if (arqSaida == NULL)               // Se nao conseguiu criar
+    {
+        printf("Problemas na CRIACAO do arquivo\n");
         return;
     }
     while (!feof(arqEntrada))
@@ -99,25 +114,30 @@ void main(void)
         fscanf(arqEntrada, "%d", &constante);
 
         printf("Constant %d\n", constante);
+        fprintf(arqSaida, "Constant %d\n", constante);
         if (constante > 0)
         {
             printf("PLUSONE\n");
-            item = add("\n", NULL);
-            item = trataNumero(constante, item);
+            fprintf(arqSaida, "PLUSONE\n");
+            item = add(arqSaida, "\n", NULL);
+            item = trataNumero(arqSaida, constante, item);
         }
         else if (constante < 0)
         {
             printf("MINUSONE\n");
-            item = add("\n", NULL);
-            item = trataNumero(constante, item);
+            fprintf(arqSaida, "MINUSONE\n");
+            item = add(arqSaida, "\n", NULL);
+            item = trataNumero(arqSaida, constante, item);
         }
         else
         {
             printf("Fim da execução pois foi colocado a Constante %d\n", constante);
+            fprintf(arqSaida, "Fim da execução pois foi colocado a Constante %d\n", constante);
             break;
         }
-        listar(item);
+        listar(arqSaida, item);
         limpaLista(item);
     }
     fclose(arqEntrada);
+    fclose(arqSaida);
 }
