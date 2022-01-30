35c363791d754a6b2dd26db261b9b0230ebe5635
unknown <lidiane.007.51@gmail.com>
2022-01-30 16:57:00 -0300

    tentando consertar problemas no windows

--- tradutorDeLinguagem/verb.c
+++ tradutorDeLinguagem/verb.c
@@ -1,8 +1,11 @@
-// ************************************************************
+// ********************
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+#define true 1
+#define false 0
 
 char *sufixo = "en";
 
@@ -26,22 +29,15 @@ char *pegaUltimosCaracteres(char *palavra, int qtd)
 {
   int tamanhoPalavra = strlen(palavra);
   int posicao = tamanhoPalavra - qtd;
-  char *novaPalavra;
-  novaPalavra = (char *)malloc(qtd * sizeof(char));
-  int cont = 0;
-  while (posicao < tamanhoPalavra)
-  {
-    novaPalavra[cont] = palavra[posicao];
-    cont++;
-    posicao++;
-  }
-  return novaPalavra;
+  char *sufuxoPalavra = (char *)malloc(qtd * sizeof(char));
+  strncpy(sufuxoPalavra, palavra + posicao, qtd);
+  return sufuxoPalavra;
 }
 
 // faz a verificacao se a palavra eh um verbo conjugado ou nao se for retorna 1 se nao retorna 0).
 int is_tempo_verbal(char *presente, char *palavra)
 {
-  return strcmp(presente, pegaUltimosCaracteres(palavra, strlen(presente))) ? 0 : 1;
+  return strcmp(presente, pegaUltimosCaracteres(palavra, strlen(presente))) ? false : true;
 }
 
 // monta a frase de acordo com o tempo verbal encontrado
@@ -54,7 +50,7 @@ void descreveTempoVerbal(FILE *arqSaida, char *sufixoLista, char *palavra, char 
 
 // faz a verificacao se a palavra eh um verbo e de qual eh o tempo verbal pertence e chama
 // o metodo para montar a frase e coloca no arquivo de saida
-void determinaTempoVerbal(FILE *arqSaida, char *palavra)
+int determinaTempoVerbal(FILE *arqSaida, char *palavra)
 {
   char presente[6][5] = {"o", "os", "a", "om", "ons", "am"};
   char passado[6][5] = {"ei", "es", "e", "em", "est", "im"};
@@ -65,63 +61,57 @@ void determinaTempoVerbal(FILE *arqSaida, char *palavra)
   int cont; //variável de controle do loop
   for (cont = 0; cont < 6; cont++)
   {
-    // // debug
-    // printf("|%s==%s>%d|%d!\n",
-    //        presente[cont],
-    //        pegaUltimosCaracteres(palavra, strlen(presente[cont])),
-    //        strcmp(presente[cont], pegaUltimosCaracteres(palavra, strlen(presente[cont]))) ? 0 : 1,
-    //        strlen(palavra));
-
-    if (is_tempo_verbal(presente[cont], palavra) == 1)
+    if (is_tempo_verbal(presente[cont], palavra) == true) // verifica se está em alguma regra de tempo verbal do presente
     {
+      // cria a frase de acordo com o tempo verbal
       descreveTempoVerbal(arqSaida, presente[cont], palavra, tempoVerbal[cont]);
-      return;
+      return true;
     }
-    else if (is_tempo_verbal(passado[cont], palavra) == 1)
+    else if (is_tempo_verbal(passado[cont], palavra) == true) // verifica se está em alguma regra de tempo verbal do passado
     {
+      // cria a frase de acordo com o tempo verbal
       descreveTempoVerbal(arqSaida, passado[cont], palavra, tempoVerbal[cont]);
-      return;
+      return true;
     }
-    else if (is_tempo_verbal(futuro[cont], palavra) == 1)
+    else if (is_tempo_verbal(futuro[cont], palavra) == true) // verifica se está em alguma regra de tempo verbal do futuro
     {
       descreveTempoVerbal(arqSaida, futuro[cont], palavra, tempoVerbal[cont]);
-      return;
+      return true;
     }
-    else if (is_tempo_verbal(sufixo, palavra) == 1)
+    else if (is_tempo_verbal(sufixo, palavra) == true) // verifica se é um verbo sem conjugacao
     {
       fprintf(arqSaida, "%s is a verb case\n", palavra);
-      return;
+      return true;
     }
   }
+  //  caso nao seja verbo mostra essa frase abaixo
   fprintf(arqSaida, "%s - not a verb case\n", palavra);
-  return;
+  return false;
 }
 
 void main()
 {
   FILE *arqEntrada;
   FILE *arqSaida;
-  char tmp[20];
-  char *palavra = malloc(strlen(tmp));
+  char palavra[20];
 
   arqEntrada = fopen("verb.in", "r"); // pega um arquivo texto para leitura
   if (arqEntrada == NULL)             // verifica se nào conseguiu encontrar
   {
-    printf("Problemas na leitura do arquivo \n");
+    printf("Problemas na leitura do arquivo verb.in\n");
   }
   arqSaida = fopen("verb.out", "w"); // pega ou cria um arquivo texto para gravação (caso ja tenha algo apaga tudo)
   if (arqSaida == NULL)              // verifica se nào conseguiu criar
   {
-    printf("Problemas na CRIACAO do arquivo\n");
+    printf("Problemas na CRIACAO do arquivo verb.out\n");
   }
 
   // percorre todo arquivo pegando cada linha (palavra) que contem até o final
   while (!feof(arqEntrada))
   {
     fscanf(arqEntrada, "%s", palavra);
-    // printf("%s", palavra);
-    // tira o espacos          // faz um tratamento pra tirar os espacos do inicio e do fim da linha para pegar apenas a palavra
-    determinaTempoVerbal(arqSaida, trimString(palavra)); // chama a funcao para executar a logica se eh ou nao verbo e conjugalo caso seja
+    // 'trimString' faz um tratamento pra tirar os espacos do inicio e do fim da linha para pegar apenas a palavra
+    determinaTempoVerbal(arqSaida, trimString(palavra)); // 'determinaTempoVerbal' chama a funcao para executar a logica se eh ou nao verbo e conjugalo caso seja
   }
   // fecha os arquivos abertos
   fclose(arqEntrada);
